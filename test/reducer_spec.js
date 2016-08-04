d5c885db7d8f4f78550b1274713ba9b1f52cdb3e
nickmask <nrmaskell@gmail.com>
2016-08-04 16:07:33 +1200

    test all reducers work with fucntions

--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -3,11 +3,11 @@ import { expect } from 'chai'
 
 import reducer from '../src/reducer'
 
-decribe('reducer', () => {
+describe('reducer', () => {
 
   it('handles SET_ENTRIES', () => {
     const initialState = Map()
-    const action = { type: 'SET_ENTRIES', entries: ['Trainspotting']}
+    const action = { type: 'SET_ENTRIES', entries: ['Trainspotting'] }
     const nextState = reducer(initialState, action)
 
     expect(nextState).to.equal(fromJS({
@@ -33,7 +33,7 @@ decribe('reducer', () => {
   it('handles vote', () => {
     const initialState = fromJS({
       vote: {
-        pair: ['Trainspotting', '28 Days Later'],
+        pair: ['Trainspotting', '28 Days Later']
       },
       entries: []
     })
@@ -46,6 +46,30 @@ decribe('reducer', () => {
         tally: {'Trainspotting': 1}
       },
       entries: []
+    }))
+  })
+
+  it('has an initial state', () => {
+    const action = {type: 'SET_ENTRIES', entries: ['Trainspotting']}
+    const nextState = reducer(undefined, action)
+    expect(nextState).to.equal(fromJS({
+      entries: ['Trainspotting']
+    }))
+  })
+
+  it('can be used with reduce', () => {
+    const actions = [
+      {type: 'SET_ENTRIES', entries: ['Trainspotting', '28 Days Later']},
+      {type: 'NEXT'},
+      {type: 'VOTE', entry: 'Trainspotting'},
+      {type: 'VOTE', entry: '28 Days Later'},
+      {type: 'VOTE', entry: 'Trainspotting'},
+      {type: 'NEXT'}
+    ]
+    const finalState = actions.reduce(reducer, Map())
+    
+    expect(finalState).to.equal(fromJS({
+      winner: 'Trainspotting'
     }))
   })
 })
